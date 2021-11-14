fe0c8cbd3535869303fa28be306e1d7dd23c1aa1
pauleom <paul.s.eom@gmail.com>
2021-11-14 23:37:46 +0000

    W2D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,54 @@
+# get '/' do
+#   File.read(File.join('app/views', 'index.html'))
+# end
+
+def humanized_time_ago(time_ago_in_minutes)
+    if time_ago_in_minutes >= 60
+      "#{time_ago_in_minutes / 60} hours ago"
+    else
+        "#{time_ago_in_minutes} minutes ago"
+    end
+end
+
 get '/' do
-    File.read(File.join('app/views', 'index.html'))
+  finstagram_post_stretch = { 
+    username: "mikasa_fan",
+    avatar_url: "/images/user_profile_mikasa.jfif",
+    photo_url: "/images/volleyball_stretch.jfif",
+    humanized_time_ago: humanized_time_ago(15),
+    like_count: 1,
+    comment_count: 1,
+    comments: [{
+        username: "mikasa_fan",
+        text: "Gotta make sure to always stretch...don't want to pull a muscle!"
+    }]
+  } 
+
+  finstagram_post_spike = { 
+    username: "spike_nation",
+    avatar_url: "/images/user_profile_net.jfif",
+    photo_url: "/images/volleyball_spike.jfif",
+    humanized_time_ago: humanized_time_ago(180),
+    like_count: 2,
+    comment_count: 1,
+    comments: [{
+        username: "spike_nation",
+        text: "That form...that height...no one is blocking that!"
+    }]
+  }
+
+  finstagram_post_gif = { 
+    username: "vball_gif_master",
+    avatar_url: "/images/user_profile_shadow.png",
+    photo_url: "https://giphy.com/embed/eJ9igIFoZcPejuVcLY",
+    humanized_time_ago: humanized_time_ago(420),
+    like_count: 3,
+    comment_count: 2,
+    comments: [{
+        username: "vball_gif_master",
+        text: "What a dig!"
+    }]
+  } 
+
+  [finstagram_post_stretch, finstagram_post_spike, finstagram_post_gif].to_s
 end
