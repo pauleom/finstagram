cec02d06c638ab8f6d488005920cd11d17a27719
pauleom <paul.s.eom@gmail.com>
2021-11-28 23:01:29 +0000

    cleaning up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,10 @@
 class User < ActiveRecord::Base
-
     has_many :finstagram_posts
     has_many :comments
     has_many :likes
-
 end
 
 class User < ActiveRecord::Base
-    
     validates :email, :username, uniqueness: true
     validates :email, :avatar_url, :username, :password, presence: true
-  
   end
