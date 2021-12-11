73d62d25fffcfc6fb785eba5a810e14b5cf85762
pauleom <paul.s.eom@gmail.com>
2021-12-11 23:26:19 +0000

    Week 5 catch up

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
@@ -17,7 +23,7 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
   
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
@@ -29,5 +35,44 @@ get '/login' do
 end
 
 post '/login' do
-  params.to_s
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id})
+
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show")
 end
